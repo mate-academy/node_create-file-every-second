ffaea54240d130025b70f860e48bc8ed5a08c51f
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-11-23 22:41:04 +0200

    Create file every second solution added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
 'use strict';
+
+import fs from 'fs';
+
+function createFile() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  const fileName = `app-${hours}_${minutes}_${seconds}.log`;
+
+  fs.writeFile(fileName, String(date), (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log("Success at " + date);
+  });
+}
+
+setInterval(createFile, 1000);
