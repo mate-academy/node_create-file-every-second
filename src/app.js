afa3897090072017307400ffa643365bd8996689
NikorychAndrii <anikorych@codifire.com>
2021-12-14 16:26:47 +0200

    Solution Node Create File Every Second

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const fs = require('fs');
+
+let sec = 0;
+let min = 0;
+let hour = 0;
+
+function createFile() {
+  fs.writeFile(
+    `./app-${hour}_${min}_${sec}.log`,
+    `${hour}:${min}:${sec}`,
+    (err) => {
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.log(err);
+      }
+
+      // eslint-disable-next-line no-console
+      console.log(`${hour}:${min}:${sec}`);
+    });
+  sec++;
+
+  if (sec >= 60) {
+    sec = 0;
+    min++;
+  }
+
+  if (min >= 60) {
+    min = 0;
+    hour++;
+  }
+}
+
+setInterval(() => {
+  createFile();
+}, 1000);
