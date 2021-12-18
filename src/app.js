1260821376b8166e6b90a63d3312a26f3a55353d
Oleg Semko <oleg.semko@zazmic.com>
2021-12-18 09:08:53 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const fs = require('fs');
+
+(function createFileEverySecond() {
+  setInterval(() => {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    const fileName = `app-${hours}_${minutes}_${seconds}.log`;
+    fs.writeFile(`app-${fileName}.log`, Date(), (err, data) => {
+      if (err) {
+        throw err;
+      }
+       // eslint-disable-next-line no-console
+      console.log(`File ${fileName} created`);
+    });
+  }, 1000);
+})()
