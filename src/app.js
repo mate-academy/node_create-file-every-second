7baa20742fd68bef2e191d1a2e9714a7c0ca3744
Andriy Bulbuk <bulbuk.andriy@gmail.com>
2022-01-24 20:41:58 +0100

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-'use strict';
+import fs from 'fs/promises';
+
+function getTimestamp(separator) {
+  const date = new Date();
+
+  return `${date.getHours()}${separator}`
+  + `${date.getMinutes()}${separator}`
+  + `${date.getSeconds()}`;
+}
+
+const fileCreator = setInterval(() => {
+  try {
+    fs.writeFile(`./app-${getTimestamp('_')}.txt`, new Date().toTimeString());
+    console.log(getTimestamp('-'));
+  } catch (error) {
+    console.log(error);
+    clearInterval(fileCreator);
+  }
+}, 1000);
