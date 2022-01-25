c8f731521fb3901642ecdffc47e803ba4fb9fa07
Alex Kostomarov <kostomarov.aleks@gmail.com>
2022-01-25 13:10:58 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const interval = 1000;
+
+const createDateFile = () => {
+  const date = new Date();
+
+  const timestamp = date.getHours()
+    + '_' + date.getMinutes()
+    + '_' + date.getSeconds();
+
+  const fileName = `app-${timestamp}.log`;
+
+  fs.writeFile(fileName, timestamp, (error) => {
+    if (error) {
+      console.log('Error! No file created.');
+    } else {
+      console.log(`Timestamp: ${timestamp}`);
+      console.log(`New file is created: ${fileName}`);
+    }
+  });
+};
+
+setInterval(() => createDateFile(), interval);
