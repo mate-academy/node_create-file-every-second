a738d9d061949efbc041c3d947905b1b2e552eb6
Dmytro  Mykoliv <dmytro.mykoliv@indeema.com>
2022-03-10 14:40:45 +0200

    resolved create file every second

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const fs = require('fs');
+
+function createDateFile() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+  const content = date.toLocaleString();
+  const fileName = `app-${hours}_${minute}_${second}.log`;
+
+  fs.writeFile(`src/${fileName}`, content, (err) => {
+    if (err) {
+      throw err;
+    }
+    // eslint-disable-next-line no-console
+    console.log(`
+      File: ${fileName} was creates at: ${date.toLocaleTimeString()}
+    `);
+  });
+};
+
+setInterval(() => {
+  createDateFile();
+}, 1000);
