9e128185658174c769a029533625ed4fe5a8c847
dant0n <dmibuc@gmail.com>
2022-04-02 15:11:28 +0300

    Finish hw.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+setInterval(() => {
+  const date = new Date();
+  const fileName
+    = `app-${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}.log`;
+
+  createLogFile(fileName, date.toTimeString());
+}, 1000);
+
+function createLogFile(name, content) {
+  fs.writeFile(name, content, error => {
+    if (error) {
+      return;
+    }
+
+    console.log(name, content);
+  });
+}
