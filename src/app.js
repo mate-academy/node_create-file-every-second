40c3c0548bd144a2f9d056c9b8d5404561494031
Vsevolod Maslak <vsevolodmaslak@gmail.com>
2022-04-26 14:17:27 +0300

    final

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 'use strict';
+const fs = require('fs');
+
+setInterval(() => {
+  let date = new Date;
+  let time = `${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}`;
+
+  fs.writeFile(`app-${time}.log`,
+    time, (err, data) => {
+      if (err) {
+      console.log(err);
+      } else {
+        console.log(`${date.getHours()}_${date.getMinutes()}_${date.getSeconds()} ${`app-${time}.log`}`);
+      }
+    });
+}, 1000);
