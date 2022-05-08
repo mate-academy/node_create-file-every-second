cedb7eb98d6e4700377be7731fdb57d900b6bd37
Bohdan Maksymiuk <bohdan.maksymiuk@ukr.net>
2022-05-08 22:26:14 +0300

    done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs/promises');
+
+setInterval(async() => {
+  const date = new Date();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+  const content = date.toString();
+
+  try {
+    await fs.writeFile(`app-${hour}-${minute}-${second}.log`, content);
+    console.log(content);
+  } catch (error) {
+    console.log('Error');
+  }
+}, 1000);
