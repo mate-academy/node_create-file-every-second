be0cebf81322f34b4fa803ffd0a27ff404001785
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2022-06-02 22:20:59 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const fs = require('fs');
+
+setInterval(() => {
+  const hours = new Date().getHours();
+  const minutes = new Date().getMinutes();
+  const seconds = new Date().getSeconds();
+
+  fs.writeFile(
+    `src/app-<${hours}>_<${minutes}>_<${seconds}>.log`,
+    `${hours}_${minutes}_${seconds}`,
+    (err, data) => {
+      if (err) {
+        return err;
+      }
+    }
+  );
+}, 1000);
