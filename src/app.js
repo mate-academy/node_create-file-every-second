9fafcbb3d08ae78ffd73723b1950f1643f494193
Dima Permiakov <permiakov.d.s@gmail.com>
2022-06-19 20:25:36 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const readline = require('readline');
+const { creator } = require('./creator');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Create files? (Yes or No): ', (userInput) => {
+  if (userInput === 'Yes' || userInput === 'yes') {
+    setInterval(() => {
+      creator();
+    }, 1000);
+  } else {
+    rl.close();
+  }
+});
