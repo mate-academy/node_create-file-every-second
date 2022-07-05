0c615a5730b34cd0f3cf59c9994cf6edd90f78e1
Alisa Ivashkevych <a.ivashkevych@mate.academy>
2022-07-05 17:06:14 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const fs = require('fs');
+
+setInterval(() => {
+  const time = new Date();
+  // eslint-disable-next-line
+  const name = `app-${time.getHours()}_${time.getMinutes()}_${time.getSeconds()}.log`;
+  const content = String(time.getTime());
+
+  fs.writeFile(name, content, (err) => {
+    if (err) {
+      // eslint-disable-next-line
+      console.log(err);
+
+      return;
+    }
+
+    // eslint-disable-next-line
+    console.log(`File with name ${name} and content ${content} was sucessfuly created`);
+  });
+}, 1000);
