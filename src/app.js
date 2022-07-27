33d267544d309da8604ed9abbaefef43bd3bedcf
Nikita Karbazin <karbazinnikita@gmail.com>
2022-07-27 17:34:29 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const fs = require('fs');
+
+async function log() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  fs.writeFile(`./src/app-${hours}_${minutes}_${seconds}.log`,
+    date + '',
+    (err) => {
+      if (!err) {
+        // eslint-disable-next-line no-console
+        console.log('Success');
+      } else {
+        // eslint-disable-next-line no-console
+        console.log('Failed');
+      }
+    });
+}
+
+setInterval(log, 1000);
