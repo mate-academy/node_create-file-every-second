41cfc8b6c0c756f9455eb878f2d09a90b32ffa61
Bohdana Usenko <usenkobogdana19@gmail.com>
2022-08-08 23:33:52 +0300

    create-file-every-second solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const fs = require('fs');
+
+setInterval(() => {
+  const time = new Date();
+  const name = `app-${time.getHours()}_${time.getMinutes()}_
+  ${time.getSeconds()}.log`;
+  const content = String(time.getTime());
+
+  fs.writeFile(`./src/${name}`, content, (err) => {
+    if (err) {
+      // eslint-disable-next-line
+      console.log(err);
+
+      return;
+    }
+
+    // eslint-disable-next-line no-console
+    console.log(`File with name ${name}
+      and content ${content} was sucessfuly created`
+    );
+  });
+}, 1000);
