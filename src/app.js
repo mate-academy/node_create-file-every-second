187f7b59833a7f34ee1072ab3b8573d6e35ae7b2
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2022-08-14 17:06:18 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const createFile = () => {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  fs.writeFile(`src/app-${hours}_${minutes}_${seconds}.log`,
+    `${date}`,
+    (error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(`Created file name:
+      app-${hours}_${minutes}_${seconds}.log\n
+      File content: ${date}`);
+      }
+    });
+};
+
+setInterval(createFile, 1000);
