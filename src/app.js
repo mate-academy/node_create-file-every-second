1db0f3fbbb3a348aa365675cac9596fc218cc100
Roman Shevchyshyn <romanshevchyshyn1998@gmail.com>
2022-08-17 11:21:21 +0300

    my first solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
 'use strict';
+/* eslint-disable no-console */
+
+const fs = require('fs');
+
+setInterval(() => {
+  const hours = new Date().getHours();
+  const minutes = new Date().getMinutes();
+  const seconds = new Date().getSeconds();
+
+  fs.writeFile(
+    `./src/app-${hours}_${minutes}_${seconds}.log`,
+    `Created at: ${hours}:${minutes}:${seconds} (GMT+3)`,
+    (err, data) => {
+      if (err) {
+        console.log(`Error: ${err}`);
+      }
+    }
+  );
+
+  // eslint-disable-next-line max-len
+  console.log(`Created at: ${hours}:${minutes}:${seconds} File: app-${hours}_${minutes}_${seconds}.log`);
+}, 1000);
