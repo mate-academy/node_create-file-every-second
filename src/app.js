29e466e064c0571c011ab8a64b2b17df5fbb3b26
Roman Kovalchuk <roma220702@gmail.com>
2022-09-10 13:55:44 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const func = () => {
+  const date = new Date();
+  const name = `./src/app-${date.getHours()}
+    -${date.getMinutes()}-${date.getSeconds()}.log`;
+
+  fs.writeFile(name, `${date.getTime()}`, (err) => {
+    if (!err) {
+      console.log(`Filename: ${name} \nContent: ${date.getTime()}`);
+    } else {
+      console.log(err);
+    }
+  });
+};
+
+setInterval(() => {
+  func();
+}, 1000);
