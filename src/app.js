6dd38d0f5971cd9d82fdbef3bfd4672afcb400b7
Bohdan Poshtarenko <mira1zu733@gmail.com>
2022-09-12 18:53:57 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const fs = require('fs');
+
+const intervalId = setInterval(() => {
+  const date = new Date();
+  const seconds = date.getSeconds();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const timestamp = String(date.getTime());
+  const fileName = `./app-${hours}_${minutes}_${seconds}.log`;
+
+  fs.writeFile(
+    fileName,
+    timestamp,
+    (err) => {
+      if (err) {
+        clearInterval(intervalId);
+        console.log('Error! Aborting.');
+
+        return;
+      }
+
+      console.log(`${timestamp}: Created file name: ${fileName}`);
+    },
+  );
+}, 1000);
