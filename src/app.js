7b05ac03a3bf7a7a364a5cff6d1a3b0876903c8d
Roman Kovalchuk <roma220702@gmail.com>
2022-09-13 19:27:19 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const fs = require('fs');
 
 const func = () => {
   const date = new Date();
-  const name = `./src/app-${date.getHours()}
+  const firstName = './src/app-';
+  const name = `${firstName}${date.getHours()}
     -${date.getMinutes()}-${date.getSeconds()}.log`;
 
   fs.writeFile(name, `${date.getTime()}`, (err) => {
