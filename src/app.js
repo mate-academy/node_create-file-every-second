e36c2b85cebfd312296d0da245a553cec17bf81b
Sasha Yatsykovych <olexandr.y@mate.academy>
2022-09-18 21:32:03 +0300

    add solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 'use strict';
+/* eslint-disable no-console */
+
+const fs = require('fs');
+
+function padTo2Digits(num) {
+  return String(num).padStart(2, '0');
+}
+
+setInterval(() => {
+  const date = new Date();
+
+  const fileName = `app-${padTo2Digits(date.getHours())}_`
+    + padTo2Digits(date.getMinutes())
+    + `_${padTo2Digits(date.getSeconds())}`;
+
+  fs.writeFile(`./src/${fileName}`, new Date().toString(), (error) => {
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    console.log('Created: ', new Date().toString());
+    console.log('File name: ', fileName);
+  });
+}, 1000);
