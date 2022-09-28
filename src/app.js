d361a5ccbbff88c2a5639f5c2ad1f4ac88a04534
Aleksandr Yuschenko <105875778+AleksandrYuschenko@users.noreply.github.com>
2022-09-28 13:34:35 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+setInterval(() => {
+  const timestamp = new Date().toLocaleTimeString();
+  const hours = timestamp.split(':')[0];
+  const minutes = timestamp.split(':')[1];
+  const seconds = timestamp.split(':')[2];
+
+  console.log(timestamp);
+
+  fs.writeFile(`./src/app-${hours}_${minutes}_${seconds}.log`,
+    timestamp,
+    er => {
+      if (er) {
+        console.log('Error', er.code);
+      }
+    });
+}, 1000);
