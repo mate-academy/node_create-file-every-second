210d6f3ddc5015f1be39eab9065c16dda7275dad
Roman Nikitenko <Lomkaaa@ukr.net>
2022-10-12 06:51:55 +0300

    Task complete

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const fs = require('fs');
+
+function clock() {
+  const data = new Date();
+  const hours = new Date().getHours();
+  const minutes = new Date().getMinutes();
+  const seconds = new Date().getSeconds();
+
+  // eslint-disable-next-line max-len
+  fs.appendFile(`./app-${hours}_${minutes}_${seconds}.log`, `${data}`, (err) => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.log(err);
+
+      return;
+    }
+
+    const plusZero = seconds < 10 ? `0${seconds}` : seconds;
+
+    // eslint-disable-next-line no-console,max-len
+    console.log(`Complete create a new file at ${hours}:${minutes}:${plusZero}`);
+  });
+
+  setTimeout(clock, 1000);
+}
+
+setTimeout(clock, 1000);
