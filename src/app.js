453eddf9c9de29aa5b28395f9513e1c3c329d5e6
Jamal Amoian <leonidagutin09@gmail.com>
2022-10-13 15:30:04 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+/* eslint-disable max-len */
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const makeFileEverySecond = () => {
+  const date = new Date();
+  const seconds = date.getSeconds();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+
+  fs.writeFile(`${hours}_${minutes}_${seconds}`, `${date.toLocaleTimeString()}`, () => {
+    console.log(date.toLocaleTimeString());
+  });
+};
+
+setInterval(() => {
+  makeFileEverySecond();
+}, 1000);
