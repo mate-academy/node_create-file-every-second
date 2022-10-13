1865a0be325f2abd69a33b54c8434836caf54598
Roman Nikitenko <Lomkaaa@ukr.net>
2022-10-13 20:29:45 +0300

    Fix

--- src/app.js
+++ src/app.js
@@ -8,20 +8,22 @@ function clock() {
   const minutes = new Date().getMinutes();
   const seconds = new Date().getSeconds();
 
-  // eslint-disable-next-line max-len
-  fs.appendFile(`./app-${hours}_${minutes}_${seconds}.log`, `${data}`, (err) => {
-    if (err) {
-      // eslint-disable-next-line no-console
-      console.log(err);
+  fs.appendFile(
+    `./app-${hours}_${minutes}_${seconds}.log`,
+    `${data}`,
+    (err) => {
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.log(err);
 
-      return;
-    }
+        return;
+      }
 
-    const plusZero = seconds < 10 ? `0${seconds}` : seconds;
+      const plusZero = seconds < 10 ? `0${seconds}` : seconds;
 
-    // eslint-disable-next-line no-console,max-len
-    console.log(`Complete create a new file at ${hours}:${minutes}:${plusZero}`);
-  });
+      // eslint-disable-next-line no-console,max-len
+      console.log(`Complete create a new file at ${hours}:${minutes}:${plusZero}`);
+    });
 
   setTimeout(clock, 1000);
 }
