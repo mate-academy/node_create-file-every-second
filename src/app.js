9732a62778d7ee5cbe0c3358b95dad7b59a5ad60
Maks Sobko <tiserettt@gmail.com>
2022-10-13 14:28:48 +0300

    implemented function to create files every second

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
+/* eslint-disable no-console */
 'use strict';
+
+const { writeFile } = require('fs');
+
+setInterval(() => {
+  const date = new Date();
+
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const timeStamp = date.getTime();
+
+  const fileName = `app-${hours}_${minutes}_${seconds}.log`;
+
+  writeFile(fileName, String(timeStamp), err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(timeStamp, fileName);
+    }
+  });
+}, 1000);
