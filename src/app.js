bcb3705d85141a6d850aaed936ea6cbc8dac77f5
Andrii Stiebalov <stiebalov@gmail.com>
2022-10-13 15:46:59 +0300

    createFileEerySecond was added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const createFile = () => {
+  const date = new Date();
+  const seconds = date.getSeconds();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const formatName = `./app-${hours}_${minutes}_${seconds}.log`;
+  const localeContent = date.toLocaleTimeString();
+
+  fs.writeFile(formatName, localeContent, (error) => {
+    if (error) {
+      console.log('error');
+    } else {
+      console.log(localeContent);
+    }
+  });
+};
+
+const createFileEerySecond = () => {
+  setInterval(() => createFile(), 100);
+};
+
+createFileEerySecond();
