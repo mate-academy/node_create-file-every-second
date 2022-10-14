5db0ed037c79a340bc96bb9cd5e4f322d7ded533
Yevhen Nikolaienko <illnino380@gmail.com>
2022-10-14 22:25:30 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,53 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const getCorrectDate = () => {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const correctHours = hours < 10
+    ? `0${hours}`
+    : hours;
+  const correctMinutes = minutes < 10
+    ? `0${minutes}`
+    : minutes;
+  const correctSeconds = seconds < 10
+    ? `0${seconds}`
+    : seconds;
+  const timeStamp = date.toTimeString().split(' ')[0];
+
+  return {
+    correctHours,
+    correctMinutes,
+    correctSeconds,
+    timeStamp,
+  };
+};
+
+const createFile = () => {
+  const {
+    correctHours,
+    correctMinutes,
+    correctSeconds,
+    timeStamp,
+  } = getCorrectDate();
+
+  const fileName = `${__dirname}/app-${correctHours}_${correctMinutes}\
+_${correctSeconds}.log`;
+
+  fs.writeFile(fileName, timeStamp, (err) => {
+    if (err) {
+      console.log('Error');
+    }
+
+    console.log(timeStamp);
+    console.log(`${fileName} is created succesfully`);
+  });
+};
+
+setInterval(() => {
+  createFile();
+}, 1000);
