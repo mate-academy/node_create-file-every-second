835fb6f1362de864230bfd44b68d40b332502c30
Olga Vasylevskaya <olgavasylevskaya@ukr.net>
2022-12-26 20:16:28 +0100

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 'use strict';
+import fs from "fs";
+
+const createFile = () => {
+  const time = new Date().toLocaleTimeString()
+  const fileName = `app-${time.replaceAll(':', '_')}.log`
+
+  fs.writeFile(`./src/${fileName}`, time, (error) => {
+    if (!error) {
+      console.log(time, fileName)
+    } else {
+      console.log(error)
+    }
+  });
+}
+
+setInterval(createFile, 1000);
