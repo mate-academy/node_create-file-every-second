c0149d624b27ec568550d9912fc90bdf594a9ead
Olga Vasylevskaya <olgavasylevskaya@ukr.net>
2022-12-27 18:01:36 +0100

    Fix linter problems

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 'use strict';
-import fs from "fs";
+
+const fs = require('fs');
 
 const createFile = () => {
-  const time = new Date().toLocaleTimeString()
-  const fileName = `app-${time.replaceAll(':', '_')}.log`
+  const time = new Date().toLocaleTimeString();
+  const fileName = `app-${time.replaceAll(':', '_')}.log`;
 
   fs.writeFile(`./src/${fileName}`, time, (error) => {
     if (!error) {
-      console.log(time, fileName)
+      // eslint-disable-next-line no-console
+      console.log(time, fileName);
     } else {
-      console.log(error)
+      // eslint-disable-next-line no-console
+      console.log(error);
     }
   });
-}
+};
 
 setInterval(createFile, 1000);
